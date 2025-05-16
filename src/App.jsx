9ad25f9153e0c0b3541374a84f583cd9bb88efb7
Rohit singh <rohitdev124421@gmail.com>
2025-05-16 12:29:25 +0530

    firtst game

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex flex-col gap-9 m-9">
+      <h1 className="bg-red-500 text-4xl text-center">hello rohit</h1>
+      <div className="h-48 w-64 text-center border-2 border-black overflow-auto">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          doloribus corporis adipisci a eaque id impedit molestias itaque
+          repudiandae? Velit voluptatibus rerum quisquam veniam illum ipsa iste
+          excepturi nobis dignissimos!lorem45 Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit. Voluptates placeat odit quia rerum sit
+          aspernatur, blanditiis, ut numquam porro architecto, ratione ab nihil
+          expedita accusantium pariatur! Ea in at dolore praesentium iusto
+          quaerat velit nesciunt ipsa officia alias, perferendis ullam
+          laboriosam inventore ad, molestiae repudiandae.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
